3efdcc18e927bf93a6bb5ab2265d98350df71ccf
mohamed <iaazi.mohamed.1998@gmail.com>
2025-03-18 15:23:59 +0000

    i create the sql query

---
+++ SQL.sql
@@ -0,0 +1,35 @@
+
+
+create  database Xconstracteur ;
+        use Xconstracteur ;
+
+
+create  table Project(
+                         projectId int primary key  auto_increment,
+                         name varchar(100) ,
+                         description varchar(200),
+                         startdate date ,
+                         enddate date ,
+                         budget int
+
+);
+
+create  table Tache(
+                       TachetId int primary key  auto_increment,
+                       tasktitle varchar(100) ,
+                       startdate date ,
+                       enddate date ,
+                       projectId int
+
+);
+
+create  table Resource(
+                          ResourceId int primary key  auto_increment,
+                          name varchar(100) ,
+                          detail varchar(200),
+                          type varchar(100) ,
+                          quantité int ,
+                          fournisseur varchar(100)
+
+);
+)
