651827de13f88135ddfb0e104d4f9d1de0ba1667
mohamed <iaazi.mohamed.1998@gmail.com>
2025-03-21 10:52:03 +0000

    create the ResourcesServlet to display all the resources

--- src/main/java/com/medo/xbuilder/Controller/ResourcesServlet.java
+++ src/main/java/com/medo/xbuilder/Controller/ResourcesServlet.java
@@ -1,5 +1,8 @@
 package com.medo.xbuilder.Controller;
 
+import com.medo.xbuilder.Model.Resource;
+import com.medo.xbuilder.Service.Resources.ResourceService;
+import com.medo.xbuilder.Service.Resources.ResourceServiceImp;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,13 +11,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet("/Resources")
 public class ResourcesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ResourceServiceImp service = new ResourceServiceImp();
         RequestDispatcher dispatcher=req.getRequestDispatcher("/views/ResourceList.jsp");
+        List<Resource> resources = service.DisplayALLResource();
+        req.setAttribute("resources", resources);
         dispatcher.forward(req,resp);
     }
 
